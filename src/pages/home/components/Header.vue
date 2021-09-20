0f7937537d1fcbb1a7de92c266f86dfaf296d15f
sunn <1239214623@qq.com  name=sunn>
2021-09-20 23:18:54 +0800

    city router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,9 +6,10 @@
       <div class="header-input">
         <span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题
       </div>
-      <div class="header-right">
-        {{this.city}}<span class="iconfont arrow-icon">&#xe64a;</span></div>
-    
+      <router-link to="/city">
+        <div class="header-right">
+            {{this.city}}<span class="iconfont arrow-icon">&#xe64a;</span></div>
+    </router-link>  
     </div>
 </template>
 
@@ -26,7 +27,7 @@
 
     .header{
         display: flex ;  
-        line-height: .86rem;
+        line-height: $headerHeight;
         background : $bgColor;
         color : #fff;
      }
@@ -53,6 +54,7 @@
         width: 1.24rem;
         float: right;
         text-align: center;
+        color: white;
     }
       .arrow-icon{
           font-size: .24rem;
