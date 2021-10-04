914368bdfda9332301bfa0789f2b26e89148d07c
sunn <1239214623@qq.com  name=sunn>
2021-10-05 06:07:54 +0800

    performance

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,17 +1,72 @@
 <template>
     <ul class="list">
-        <li class="item">A</li>
-        <li class="item">A</li>
-        <li class="item">A</li>
-        <li class="item">A</li>
-        <li class="item">A</li>
+        <li 
+        class="item" 
+        v-for="item of letters"
+        :key="item"
+        @click="handleLetterClick"
+        @touchStart = "handleTouchStart"
+        @touchMove = "handleTouchMove"
+        @touchEnd = "handleTouchEnd"
+        :ref = "item">
+            {{item}}
+        </li>
     </ul>
   
 </template>
 
 <script>
     export default{
-        name:'CityAlphabet'
+        name:'CityAlphabet',
+        props:{
+            cities:Object
+        },
+        computed:{
+            letters(){
+                const letters=[];
+                for (let i in this.cities) {
+                    letters.push(i);
+            }
+                return letters;
+            }
+            
+        },
+        data(){
+            return{
+                touchStatus:false,
+                startY : 0,
+                timer:null
+            }
+        } ,
+        updated(){
+           this.startY = this.$refs['A'][0].offsetTop;
+        },
+        methods:{
+            handleLetterClick(e){
+                this.$emit("change",e.target.innerText)   
+            },
+            handleTouchStart(){
+                this.touchStatus = true;
+            },
+            handleTouchMove(){
+                if (this.touchStatus) {
+                    if (this.timer) {
+                        clearTimeout(this.timer)
+                    }
+                    this.timer = setTimeout(() =>{
+                        const touchY = e.touches[0].clientY - 79;
+                        const index =  Math.floor((touchY - this.startY)/20);
+                        if(index>=0 && index<this.letters.length) {
+                            this.$emit('chang',this.letters[index])
+                        }
+                    },16)
+                   
+                }
+            },
+            handleTouchEnd(){
+                this.touchStatus = false;
+            }
+        }
     }
 </script>
 
@@ -31,6 +86,6 @@
      .item{
          text-align: center;
          line-height: .4rem;
-         color: $bgCOlor;
+         color: $bgColor;
      }
 </style>
